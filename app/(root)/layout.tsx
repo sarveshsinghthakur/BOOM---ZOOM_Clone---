a8db6f60ba61b1918f1c0a30207d653256abcc72
Sarveshsingh09 <sarvesh8882589477@gmail.com>
2024-08-10 17:31:46 +0530

    init

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,136 @@
+// codewithsarvesh
+import { ReactNode } from 'react';
+
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// codewithsarvesh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// codewithsarvesh
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        dark:{
+          1: "#1C1F2E",
+          2: "#161925",
+          3: "#ecf0f1",
+          4: "#3498db", 
+        },
+        sky:{
+          1:'#C9DDFF',
+          2:'#ECF0FF',
+          3:'#F5FCFF',
+
+        },
+        orange:{
+          1: "#FF742E",
+        },
+        purple:{
+          1: "#830EF9",
+        },
+        yellow:{
+          1: "#F9A90E",
+        },
+        blue:{
+          1:'#0E78F9'
+        },
+      },
       backgroundImage: {
+        hero:"url('/images/hero-background.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
